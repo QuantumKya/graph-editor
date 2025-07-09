bda81cef7f82e9854fba812108d2d1f167089436
QuantumKya <rohan.vats4nier@gmail.com>
2025-07-08 23:53:02 -0500

    github actions page setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -12,7 +14,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
+			base: dev ? '' : process.env.BASE_PATH,
 		}
 	}
 };
